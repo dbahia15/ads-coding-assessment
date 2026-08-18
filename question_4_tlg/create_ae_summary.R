c0b1473f9115527c88753c01727b9037b806e573
Dilpreet <317932061+dbahia15@users.noreply.github.com>
2026-08-18 12:56:17 +0000

    Create treatment-emergent adverse events table

---
+++ question_4_tlg/create_ae_summary.R
@@ -0,0 +1,40 @@
+# Question 4: TLG Adverse Events Reporting
+#
+# Creates a treatment-emergent adverse-event summary table using {gtsummary}.
+
+### Set Up ###
+
+# Load packages
+library(dplyr)
+library(tidyr)
+library(gtsummary)
+library(pharmaverseadam)
+
+# Read ADaM input datasets
+adae <- pharmaverseadam::adae
+adsl <- pharmaverseadam::adsl
+
+# Filter to treatment-emergent AEs
+adae_teae <- adae %>%
+  filter(TRTEMFL == "Y")
+
+### Create treatment-emergent AE summary table ###
+tbl_ae <- adae_teae |>
+  tbl_hierarchical(
+    variables = c(AESOC, AETERM),
+    by = ACTARM,
+    denominator = adsl,
+    id = USUBJID,
+    overall_row = TRUE,
+    label = list(..ard_hierarchical_overall.. = "Treatment-Emergent Adverse Events"
+    )
+  ) |>
+  sort_hierarchical()
+
+### Save the table as an HTML file ###
+
+tbl_ae |>
+  as_gt() |>
+  gt::gtsave(filename = "question_4_tlg/ae_summary_table.html")
+
+cat("ae_summary_table.html saved")
